182735d3c88ad87494c0edf79be9bf120bd133f7
Razal <razalbinkabeer01@gmail.com>
2023-05-22 08:20:02 +0400

    Resizing images

--- images.py
+++ images.py
@@ -1,8 +1,14 @@
 from PIL import Image, ImageFilter
 
-img = Image.open("./images/pikachu.jpg")
-filtered_img = img.convert("L")
-crooked = filtered_img.rotate(90)
-crooked.save("gray.png", "png")
+img = Image.open("./images/astro.jpg")
+# filtered_img = img.convert("L")
+# crooked = filtered_img.rotate(90)
 
-crooked.show() # show the image
+
+# crooked.save("gray.png", "png")
+
+# crooked.show() # show the image
+
+# resized_img = img.resize((650, 650))
+img.thumbnail((400, 400)) # maintains the aspect ratio
+img.save("new-img.png", "png")
